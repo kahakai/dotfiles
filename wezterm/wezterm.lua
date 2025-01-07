8bbc66dd904a49d2d521a449d9c0c332b35a9508
kahakai <git@kahakai.me>
2025-01-07 20:12:32 +0000

    feat: add dotfiles for WezTerm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.keys = {
+	{
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "n",
+		mods = "ALT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "n",
+		mods = "SHIFT|ALT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+return config

4996acb673bd54bdf14c8919a95b36926bfc4cee
kahakai <git@kahakai.me>
2025-12-22 16:48:38 +0000

    feat(wezterm): remove redundant config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,11 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.keys = {
-	{
-		key = "x",
-		mods = "CTRL",
-		action = wezterm.action.CloseCurrentPane({ confirm = false }),
-	},
+	-- New Pane
 	{
 		key = "n",
 		mods = "ALT",
@@ -17,6 +13,19 @@ config.keys = {
 		key = "n",
 		mods = "SHIFT|ALT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+
+	-- Navigane Panes
+	{ key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+
+	-- Close Pane
+	{
+		key = "x",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
 	},
 }
 

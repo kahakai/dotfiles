30422fcb68ed24ac3f5c23fe8d34bf977bc2617b
kahakai <git@kahakai.me>
2026-03-31 14:17:13 +0100

    feat(macos): remove language indicator setting

--- macos.sh
+++ macos.sh
@@ -5,6 +5,3 @@ defaults -currentHost write -g AppleFontSmoothing -int 0
 
 # Hide action images in menus
 defaults write -g NSMenuEnableActionImages -bool NO
-
-# Disable language input source indicator popup
-defaults write -g TSMLanguageIndicatorEnabled 0
